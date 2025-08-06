95ceb50c10507d26d1fb2073b803da96caaeaaed
salitooooo <salodorado2004@gmail.com>
2025-08-06 09:29:33 -0500

    . borre oroyecto

--- actividad1/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module co.edu.poli.actividad1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens co.edu.poli.actividad1.vista to javafx.fxml;
-    exports co.edu.poli.actividad1.vista;
-}
